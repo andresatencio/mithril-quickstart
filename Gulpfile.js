60fce93507a707a78130ccc0c1768a440ea4a7a0
Andres Atencio <and.atencio@gmail.com>
2015-02-20 08:30:41 -0300

    gulp babel (6to5)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp')
-var msx = require('gulp-msx')
+var gulp = require('gulp');
+var msx = require('gulp-msx');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var babel = require("gulp-babel");
 
 gulp.task('transform-jsx', function() {
   return gulp.src('./client/jsx/*.jsx')
@@ -16,6 +17,15 @@ gulp.task('browserify', function() {
     .pipe(gulp.dest('./build'))
 });
 
+gulp.task('babel', function () {
+	return gulp.src('./build/bundle.js')
+		.pipe(babel())
+		.pipe(rename('bundle-es6.js'))
+    	.pipe(gulp.dest('./build'))
+})
+
 gulp.task('default', ['transform-jsx']);
 
 gulp.task('build', ['transform-jsx', 'browserify']);
+
+gulp.task('es6', ['transform-jsx', 'browserify', 'babel'])
